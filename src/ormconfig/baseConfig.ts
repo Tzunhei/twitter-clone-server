6cd3d9f0d180c0498695ddcaae8e5676bb6d522d
Clément Yam <yamtzunhei@gmail.com>
2021-02-17 15:11:11 +0100

    dockerise database

--- src/ormconfig/baseConfig.ts
+++ src/ormconfig/baseConfig.ts
@@ -3,8 +3,8 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'clementyam',
+  port: Number(process.env.DB_PORT) || 54032,
+  username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'admin',
   database: process.env.DB_NAME || 'twitter-clone',
   entities: [__dirname + '/../modules/**/*.entity{.ts,.js}'],
