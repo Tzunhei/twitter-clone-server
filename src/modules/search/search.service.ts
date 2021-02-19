34b6e046929890b92b8d2100eae602c33daa49ab
Clément Yam <yamtzunhei@gmail.com>
2021-02-19 11:13:44 +0100

    send error message if results not found

--- src/modules/search/search.service.ts
+++ src/modules/search/search.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { HashtagsService } from '../hashtags/hashtags.service';
 import { UsersService } from '../users/users.service';
 
@@ -12,6 +12,9 @@ export class SearchService {
   async search(search: string) {
     const hashtags = await this.hashtagsService.searchHashtags(search);
     const users = await this.usersService.searchUsers(search);
+    if (!hashtags.length && !users.length) {
+      throw new BadRequestException(`No results for ${search}`);
+    }
     return [{ hashtags }, { users }];
   }
 }
